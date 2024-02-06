e9cdfebc00d6a3da25e96f3611faad5286f590cb
Rafael <rflpeq.rs@gmail.com>
2024-02-06 09:51:19 -0300

    projeto finalizado

--- src/main/java/com/betrybe/agrix/controller/CropController.java
+++ src/main/java/com/betrybe/agrix/controller/CropController.java
@@ -11,6 +11,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.annotation.Secured;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -33,6 +34,7 @@ public class CropController {
   }
 
   @GetMapping()
+  @Secured({"ROLE_ADMIN", "ROLE_MANAGER"})
   public List<CropDto> findAll() {
     return cropService.findAll().stream().map(CropDto::fromEntity).toList();
   }
--- src/main/java/com/betrybe/agrix/controller/FertilizerController.java
+++ src/main/java/com/betrybe/agrix/controller/FertilizerController.java
@@ -8,6 +8,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.annotation.Secured;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -36,6 +37,7 @@ public class FertilizerController {
   }
 
   @GetMapping()
+  @Secured("ROLE_ADMIN")
   public List<FertilizerDto> findAll() {
     List<Fertilizer> fertilizers = fertilizerService.findAll();
     return fertilizers.stream().map(FertilizerDto::fromEntity).toList();
